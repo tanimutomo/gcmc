9dbc8bd2000c51638745cead5489b5d16e8c6721
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-01-23 00:16:00 +0900

    make train work, should fix model's matrix calc

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import torch
+import torch.nn as nn
 
 from dataset import MCDataset
 from model import GAE
-from utils import nll_loss
+from utils import calc_rmse
 
 
 class Trainer:
@@ -13,50 +14,95 @@ class Trainer:
         self.hidden_size = [500, 75]
         self.num_basis = 2
         self.drop_prob = 0.7
+        self.epochs = 100
 
-    def create_dataset(self):
+    def set_dataset(self):
+        pass
         # self.dataset = MCDataset(root='./data/ml-100k', name='ml-100k')
-        self.dataset = MCDataset(self.root, self.dataset_name)
         # self.num_train_users = dataset.num_train_users
         # self.num_train_items = dataset.num_train_items
         # self.num_test_users = dataset.num_test_users
         # self.num_test_items = dataset.num_test_items
 
-        self.train_data, self.test_data = self.dataset[0][0], self.dataset[1][0]
-        self.train_data = self.train_data.to(device)
-        self.test_data = self.test_data.to(device)
+        # self.train_data, self.test_data = self.dataset[0][0], self.dataset[1][0]
+        # self.train_data = self.train_data.to(self.device)
+        # self.test_data = self.test_data.to(self.device)
 
-    def training_setting(self):
-        model = GAE(
-                dataset.num_train_users,
-                self.hidden_size[0]
-                self.hidden_size[1]
+    def train_setting(self):
+        dataset = MCDataset(self.root, self.dataset_name)
+        print(dataset[0])
+        self.data = dataset[0].to(self.device)
+        self.model = GAE(
+                dataset.num_nodes,
+                self.hidden_size[0],
+                self.hidden_size[1],
                 self.num_basis,
                 dataset.num_relations,
-                self.drop_out
+                self.data.num_users,
+                self.drop_prob
                 )
+        self.model = self.model.to(self.device)
 
-        criterion = nll_loss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=0.005)
+        self.criterion = nn.NLLLoss()
+        self.optimizer = torch.optim.Adam(
+                self.model.parameters(), lr=0.01, weight_decay=0.005)
+
 
     def iterate(self):
-        pass
+        for epoch in range(self.epochs):
+            loss, train_rmse = self.train()
+            if epoch % 10 == 0:
+                test_rmse = self.test()
+                self.summary(epoch, loss, train_rmse, test_rmse)
+            else:
+                self.summary(epoch, loss, train_rmse)
+
+        print('END TRAINING')
+
 
     def train(self):
-        model.train()
-
-        out = model(
-                self.train_data.x,
-                self.train_data.edge_index,
-                self.train_data.edge_type,
-                self.train_data.edge_norm
+        self.model.train()
+        self.optimizer.zero_grad()
+        out = self.model(
+                self.data.x, self.data.edge_index,
+                self.data.edge_type, self.data.edge_norm
                 )
+        loss = self.criterion(out[self.data.train_idx], self.data.train_gt)
+        loss.backward()
+        optimizer.step()
+
+        rmse = calc_rmse(out[self.data.train_idx], self.data.train_gt)
+
+        return loss.item(), rmse.item()
 
 
     def test(self):
-        pass
+        model.eval()
+        out = self.model(
+                self.data.x, self.data.edge_index, 
+                self.data.edge_type, self.data.edge_norm
+                )
 
-    def summary(self):
-        pass
+        rmse = calc_rmse(out[self.data.test_idx], self.data.test_gt)
+
+        return rmse.item()
 
 
+    def summary(self, epoch, loss, train_rmse, test_rmse=None):
+        if test_rmse is not None:
+            print('[ Epoch: {} / Loss: {} / RMSE: {} ]'.format(
+                epoch, loss, train_rmse))
+        else:
+            print('')
+            print('[ Epoch: {} / Loss: {} / RMSE: {} / Test RMSE: {} ]'.format(
+                epoch, loss, train_rmse, test_rmse))
+            print('')
+            
+        
+
+if __name__ == '__main__':
+    root = 'data/ml-100k'
+    name = 'ml-100k'
+    trainer = Trainer(root, name)
+    trainer.train_setting()
+    trainer.iterate()
