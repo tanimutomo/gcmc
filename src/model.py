fc879087838f286b7db5b5e86fcdc73dfb47376b
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-01-27 16:32:59 +0900

    move Layers to layers.py

--- src/model.py
+++ src/model.py
@@ -1,15 +1,15 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn.conv import MessagePassing
+from src.layers import RGCLayer, DenseLayer
 
 
 class GAE(nn.Module):
     def __init__(self, in_c, hid_c, out_c, num_basis, num_relations, 
-            num_user, drop_prob, ster, weight_init):
+            num_user, drop_prob, ster, weight_init, accum):
         super(GAE, self).__init__()
         self.gcenc = GCEncoder(in_c, hid_c, out_c, num_relations, num_user, 
-                drop_prob, ster, weight_init)
-        self.bidec = BiDecoder(out_c, num_basis, num_relations, ster, weight_init)
+                drop_prob, ster, weight_init, accum)
+        self.bidec = BiDecoder(out_c, num_basis, num_relations, ster, weight_init, accum)
 
     def forward(self, x, edge_index, edge_type, edge_norm):
         u_features, i_features = self.gcenc(x, edge_index, edge_type, edge_norm)
@@ -19,14 +19,15 @@ class GAE(nn.Module):
 
 
 class GCEncoder(nn.Module):
-    def __init__(self, in_c, hid_c, out_c, num_relations, num_user, drop_prob, ster, weight_init):
+    def __init__(self, in_c, hid_c, out_c, num_relations, num_user, drop_prob, ster, weight_init, accum):
         super(GCEncoder, self).__init__()
         self.num_user = num_user
-        self.rgc_layer = RGCLayer(in_c, hid_c, num_relations, drop_prob, ster, weight_init)
-        self.dense_layer = DenseLayer(hid_c, out_c, drop_prob, in_c, num_user, weight_init)
+        self.rgc_layer = RGCLayer(in_c, hid_c, num_relations, drop_prob, ster, weight_init, accum)
+        self.dense_layer = DenseLayer(hid_c, out_c, num_relations, drop_prob, in_c, num_user, weight_init, accum)
 
     def forward(self, x, edge_index, edge_type, edge_norm):
         features = self.rgc_layer(x, edge_index, edge_type, edge_norm)
+        print(features.shape)
         u_features, i_features = self.separate_features(features)
         u_features, i_features = self.dense_layer(u_features, i_features)
 
@@ -39,105 +40,15 @@ class GCEncoder(nn.Module):
         return u_features, i_features
 
 
-
-class RGCLayer(MessagePassing):
-    def __init__(self, in_c, out_c, num_relations, drop_prob, ster, weight_init):
-        super(RGCLayer, self).__init__()
-        self.in_c = in_c
-        self.out_c = out_c
-        self.num_relations = num_relations
-        self.drop_prob = drop_prob
-        self.ster = ster
-        self.weight_init = weight_init
-        
-        ord_basis = [nn.Parameter(torch.Tensor(1, in_c * out_c)) for r in range(num_relations)]
-        self.ord_basis = nn.ParameterList(ord_basis)
-        self.bn = nn.BatchNorm1d(self.in_c)
-        self.relu = nn.ReLU()
-
-        self.reset_parameters()
-
-
-    def reset_parameters(self):
-        for basis in self.ord_basis:
-            basis = self.weight_init(self.ster, basis)
-
-    def forward(self, x, edge_index, edge_type, edge_norm=None):
-        return self.propagate('add', edge_index, x=x, edge_type=edge_type, edge_norm=edge_norm)
-
-    def message(self, x_j, edge_type, edge_norm):
-        # create weight using ordinal weight sharing
-        for relation in range(self.num_relations):
-            if relation == 0:
-                weight = self.ord_basis[relation]
-            else:
-                weight = torch.cat((weight, weight[-1] 
-                    + self.ord_basis[relation]), 0)
-
-        weight = weight.reshape(-1, self.out_c)
-        weight = self.node_dropout(weight)
-        index = edge_type * self.in_c + x_j
-        out = weight[index]
-
-        return out if edge_norm is None else out * edge_norm.reshape(-1, 1)
-
-    def update(self, aggr_out):
-        # aggr_out has shape [N, out_channles]
-        aggr_out = self.bn(aggr_out.unsqueeze(0))
-        aggr_out = self.relu(aggr_out)
-        return aggr_out.squeeze(0)
-
-    def node_dropout(self, weight):
-        drop_mask = torch.rand(self.in_c) + (1 - self.drop_prob)
-        drop_mask = torch.floor(drop_mask).type(torch.float)
-        drop_mask = torch.cat([drop_mask 
-            for r in range(self.num_relations)], 0).unsqueeze(1)
-
-        drop_mask = drop_mask.expand(drop_mask.size(0), self.out_c)
-
-        assert weight.shape == drop_mask.shape
-        weight = weight * drop_mask
-
-        return weight
-
-
-
-class DenseLayer(nn.Module):
-    def __init__(self, in_c, out_c, drop_prob, num_nodes, num_user, weight_init, bias=False):
-        super(DenseLayer, self).__init__()
-        self.num_nodes = num_nodes
-        self.num_user = num_user
-
-        self.dropout = nn.Dropout(drop_prob)
-        self.fc = nn.Linear(in_c, out_c, bias=bias)
-        self.bn_u = nn.BatchNorm1d(num_user)
-        self.bn_i = nn.BatchNorm1d(num_nodes - num_user)
-        self.relu = nn.ReLU()
-
-    def forward(self, u_features, i_features):
-        u_features = self.dropout(u_features)
-        u_features = self.fc(u_features)
-        u_features = self.bn_u(
-                u_features.unsqueeze(0)).squeeze()
-        u_features = self.relu(u_features)
-
-        i_features = self.dropout(i_features)
-        i_features = self.fc(i_features)
-        i_features = self.bn_i(
-                i_features.unsqueeze(0)).squeeze()
-        i_features = self.relu(i_features)
-
-        return u_features, i_features
-
-
 class BiDecoder(nn.Module):
-    def __init__(self, feature_dim, num_basis, num_relations, ster, weight_init):
+    def __init__(self, feature_dim, num_basis, num_relations, ster, weight_init, accum):
         super(BiDecoder, self).__init__()
         self.num_basis = num_basis
         self.num_relations = num_relations
         self.weight_init = weight_init
         self.feature_dim = feature_dim
         self.ster = ster
+        self.accum = accum
 
         self.basis_matrix = nn.Parameter(
                 torch.Tensor(num_basis, feature_dim * feature_dim))
@@ -153,19 +64,34 @@ class BiDecoder(nn.Module):
             self.weight_init(self.ster, coef)
 
     def forward(self, u_features, i_features):
+        if self.accum == 'stack':
+            u_features = u_features.reshape(self.num_relations, -1, feature_dim)
+            i_features = i_features.reshape(self.num_relations, -1, feature_dim)
+            
         for relation in range(self.num_relations):
             q_matrix = torch.sum(self.coefs[relation].unsqueeze(1) * self.basis_matrix, 0)
             q_matrix = q_matrix.reshape(self.feature_dim, self.feature_dim)
             if relation == 0:
-                out = torch.chain_matmul(
-                        u_features, q_matrix, i_features.t()).unsqueeze(-1)
+                if self.accum == 'stack':
+                    out = torch.chain_matmul(
+                            u_features[relation], q_matrix, 
+                            i_features[relation].t()).unsqueeze(-1)
+                else:
+                    out = torch.chain_matmul(
+                            u_features, q_matrix, i_features.t()).unsqueeze(-1)
             else:
-                out = torch.cat((out, torch.chain_matmul(
-                    u_features, q_matrix, i_features.t()).unsqueeze(-1)),dim=2)
+                if self.accum == 'stack':
+                    out = torch.cat((out, torch.chain_matmul(
+                        u_features[relation], q_matrix, 
+                        i_features[relation].t()).unsqueeze(-1)),dim=2)
+                else:
+                    out = torch.cat((out, torch.chain_matmul(
+                        u_features, q_matrix, i_features.t()).unsqueeze(-1)),dim=2)
 
         out = out.view(u_features.shape[0] * i_features.shape[0], -1)
 
         return out
+
 
 if __name__ == '__main__':
     in_c = 2625
